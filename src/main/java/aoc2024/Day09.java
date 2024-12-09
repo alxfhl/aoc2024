809682f3748f4c0604a8c4cc8ec0b4bbff0a0d32
alxfhl <110429902+alxfhl@users.noreply.github.com>
2024-12-09 09:17:30 +0100

    Day 09

--- src/main/java/aoc2024/Day09.java
+++ src/main/java/aoc2024/Day09.java
@@ -1,16 +1,110 @@
 package aoc2024;
 
+import java.util.ArrayList;
 import java.util.List;
 
 public class Day09 {
 
     public static long getPart1(List<String> lines) {
-        return 0;
+        if (lines.size() != 1) {
+            throw new AssertionError("only one line expected");
+        }
+        List<Integer> blocks = parseBlocks(lines.getFirst());
+        defrag1(blocks);
+        return getChecksum(blocks);
     }
 
-
     public static long getPart2(List<String> lines) {
-        return 0;
+        if (lines.size() != 1) {
+            throw new AssertionError("only one line expected");
+        }
+        List<Integer> blocks = parseBlocks(lines.getFirst());
+        defrag2(blocks);
+        return getChecksum(blocks);
+    }
+
+    private static void defrag1(List<Integer> blocks) {
+        int last = blocks.size() - 1;
+        for (int i = 0; i < last; i++) {
+            if (blocks.get(i) == null) {
+                blocks.set(i, blocks.get(last));
+                blocks.set(last, null);
+                while (blocks.get(last) == null) {
+                    last--;
+                }
+            }
+        }
+    }
+
+    private static void defrag2(List<Integer> blocks) {
+        int fileId = blocks.stream().mapToInt(i -> i == null ? 0 : i).max().getAsInt();
+        while (fileId >= 0) {
+            int start = 0;
+            int end = 0;
+            for (int i = 0; i < blocks.size(); i++) {
+                if (blocks.get(i) != null && blocks.get(i) == fileId) {
+                    if (start == 0) {
+                        start = i;
+                    }
+                    end = i;
+                }
+            }
+            int emptyStart = 0;
+            int emptyEnd = 0;
+            for (int i = 0; i < blocks.size(); i++) {
+                if (i > start) {
+                    break;
+                }
+                if (blocks.get(i) == null) {
+                    if (emptyStart == 0) {
+                        emptyStart = i;
+                    }
+                    emptyEnd = i;
+                    if (emptyEnd - emptyStart == end - start) {
+                        for (int b = 0; b <= end - start; b++) {
+                            blocks.set(emptyStart + b, fileId);
+                            blocks.set(start + b, null);
+                        }
+                        break;
+                    }
+                } else {
+                    emptyStart = 0;
+                }
+            }
+            fileId--;
+        }
+    }
+
+    private static long getChecksum(List<Integer> blocks) {
+        long sum = 0;
+        for (int i = 0; i < blocks.size(); i++) {
+            Integer fileId = blocks.get(i);
+            if (fileId != null) {
+                sum += (long) fileId * (long) i;
+            }
+        }
+        return sum;
+    }
+
+    private static List<Integer> parseBlocks(String line) {
+        List<Integer> blocks = new ArrayList<>();
+        boolean fileNext = true;
+        int fileId = 0;
+        for (char ch : line.toCharArray()) {
+            int amount = ch - '0';
+            if (fileNext) {
+                for (int i = 0; i < amount; i++) {
+                    blocks.add(fileId);
+                }
+                fileId++;
+            } else {
+                for (int i = 0; i < amount; i++) {
+                    blocks.add(null);
+                }
+            }
+            fileNext = !fileNext;
+        }
+        return blocks;
     }
 
 }
--- src/test/java/aoc2024/Day09Test.java
+++ src/test/java/aoc2024/Day09Test.java
@@ -12,30 +12,30 @@ import static org.assertj.core.api.Assertions.assertThat;
 public class Day09Test {
 
     String example = """
-            
+            2333133121414131402
             """;
 
     @Test
     @Order(1)
     public void examplePart1() {
-        assertThat(Day09.getPart1(Input.fromString(example))).isEqualTo(-1);
+        assertThat(Day09.getPart1(Input.fromString(example))).isEqualTo(1928);
     }
 
     @Test
     @Order(2)
     public void puzzlePart1() {
-        assertThat(Day09.getPart1(Input.forDay(Day09.class))).isEqualTo(-1);
+        assertThat(Day09.getPart1(Input.forDay(Day09.class))).isEqualTo(6382875730645L);
     }
 
     @Test
     @Order(3)
     public void examplePart2() {
-        assertThat(Day09.getPart2(Input.fromString(example))).isEqualTo(-1);
+        assertThat(Day09.getPart2(Input.fromString(example))).isEqualTo(2858);
     }
 
     @Test
     @Order(4)
     public void puzzlePart2() {
-        assertThat(Day09.getPart2(Input.forDay(Day09.class))).isEqualTo(-1);
+        assertThat(Day09.getPart2(Input.forDay(Day09.class))).isEqualTo(6420913943576L);
     }
 }
