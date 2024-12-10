708866e81f0c7f9b052492b5b98344e050543c54
alxfhl <110429902+alxfhl@users.noreply.github.com>
2024-12-10 08:45:20 +0100

    Day 10

--- src/main/java/aoc2024/Day10.java
+++ src/main/java/aoc2024/Day10.java
@@ -1,16 +1,88 @@
 package aoc2024;
 
-import java.util.List;
+import aoc2024.tools.CharMatrix;
+import aoc2024.tools.Coord2D;
+import aoc2024.tools.Direction;
+
+import java.util.*;
 
 public class Day10 {
 
     public static long getPart1(List<String> lines) {
-        return 0;
+        CharMatrix matrix = CharMatrix.valueOf(lines);
+        List<Coord2D> candidates = matrix.findAll('0');
+        Map<Coord2D, Integer> trailheads = new HashMap<>();
+        for (Coord2D candidate : candidates) {
+            Set<Coord2D> summits = new HashSet<>();
+
+            Set<Coord2D> positions = new HashSet<>();
+            positions.add(candidate);
+            Set<Coord2D> nextPositions = new HashSet<>();
+            while (!positions.isEmpty()) {
+                for (Coord2D pos : positions) {
+                    char height = matrix.get(pos);
+                    if (height == '9') {
+                        summits.add(pos);
+                        continue;
+                    }
+                    for (Direction dir : Direction.values()) {
+                        Coord2D neighbor = pos.go(dir);
+                        if (matrix.isInside(neighbor) && matrix.get(neighbor) == (height + 1)) {
+                            nextPositions.add(neighbor);
+                        }
+                    }
+                }
+                positions = nextPositions;
+                nextPositions = new HashSet<>();
+            }
+
+            if (!summits.isEmpty()) {
+                trailheads.put(candidate, summits.size());
+            }
+        }
+        return trailheads.values().stream().mapToLong(i -> i).sum();
     }
 
 
     public static long getPart2(List<String> lines) {
-        return 0;
+        CharMatrix matrix = CharMatrix.valueOf(lines);
+        List<Coord2D> candidates = matrix.findAll('0');
+        Map<Coord2D, Long> trailheads = new HashMap<>();
+        for (Coord2D candidate : candidates) {
+            Map<Coord2D, Long> summits = new HashMap<>();
+
+            Map<Coord2D, Long> positions = new HashMap<>();
+            positions.put(candidate, 1L);
+            Map<Coord2D, Long> nextPositions = new HashMap<>();
+            while (!positions.isEmpty()) {
+                for (var entry : positions.entrySet()) {
+                    Coord2D pos = entry.getKey();
+                    long trails = entry.getValue();
+                    char height = matrix.get(pos);
+
+                    if (height == '9') {
+                        summits.compute(pos, (p, value) -> value == null ? trails : value + trails);
+                        continue;
+                    }
+
+                    for (Direction dir : Direction.values()) {
+                        Coord2D neighbor = pos.go(dir);
+                        if (matrix.isInside(neighbor) && matrix.get(neighbor) == (height + 1)) {
+                            nextPositions.compute(neighbor, (p, value) -> value == null
+                                    ? trails
+                                    : value + trails);
+                        }
+                    }
+                }
+                positions = nextPositions;
+                nextPositions = new HashMap<>();
+            }
+
+            if (!summits.isEmpty()) {
+                trailheads.put(candidate, summits.values().stream().mapToLong(i -> i).sum());
+            }
+        }
+        return trailheads.values().stream().mapToLong(i -> i).sum();
     }
 
 }
--- src/test/java/aoc2024/Day14Test.java
+++ src/test/java/aoc2024/Day14Test.java
@@ -9,40 +9,33 @@ import org.junit.jupiter.api.TestMethodOrder;
 import static org.assertj.core.api.Assertions.assertThat;
 
 @TestMethodOrder(MethodOrderer.OrderAnnotation.class)
-public class Day10Test {
+public class Day14Test {
 
     String example = """
-            89010123
-            78121874
-            87430965
-            96549874
-            45678903
-            32019012
-            01329801
-            10456732
+            
             """;
 
     @Test
     @Order(1)
     public void examplePart1() {
-        assertThat(Day10.getPart1(Input.fromString(example))).isEqualTo(36);
+        assertThat(Day14.getPart1(Input.fromString(example))).isEqualTo(-1);
     }
 
     @Test
     @Order(2)
     public void puzzlePart1() {
-        assertThat(Day10.getPart1(Input.forDay(Day10.class))).isEqualTo(472);
+        assertThat(Day14.getPart1(Input.forDay(Day14.class))).isEqualTo(-1);
     }
 
     @Test
     @Order(3)
     public void examplePart2() {
-        assertThat(Day10.getPart2(Input.fromString(example))).isEqualTo(81);
+        assertThat(Day14.getPart2(Input.fromString(example))).isEqualTo(-1);
     }
 
     @Test
     @Order(4)
     public void puzzlePart2() {
-        assertThat(Day10.getPart2(Input.forDay(Day10.class))).isEqualTo(969);
+        assertThat(Day14.getPart2(Input.forDay(Day14.class))).isEqualTo(-1);
     }
 }
