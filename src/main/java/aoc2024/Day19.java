02575ec528a2e259e0d4accb0d8e5b362266a2ab
alxfhl <110429902+alxfhl@users.noreply.github.com>
2024-12-15 10:05:24 +0100

    Prepare remaining days

---
+++ src/main/java/aoc2024/Day19.java
@@ -0,0 +1,16 @@
+package aoc2024;
+
+import java.util.List;
+
+public class Day19 {
+
+    public static long getPart1(List<String> lines) {
+        return 0;
+    }
+
+
+    public static long getPart2(List<String> lines) {
+        return 0;
+    }
+
+}
---
+++ src/test/java/aoc2024/Day18Test.java
@@ -0,0 +1,41 @@
+package aoc2024;
+
+import aoc2024.tools.Input;
+import org.junit.jupiter.api.MethodOrderer;
+import org.junit.jupiter.api.Order;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.TestMethodOrder;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+@TestMethodOrder(MethodOrderer.OrderAnnotation.class)
+public class Day18Test {
+
+    String example = """
+            
+            """;
+
+    @Test
+    @Order(1)
+    public void examplePart1() {
+        assertThat(Day18.getPart1(Input.fromString(example))).isEqualTo(-1);
+    }
+
+    @Test
+    @Order(2)
+    public void puzzlePart1() {
+        assertThat(Day18.getPart1(Input.forDay(Day18.class))).isEqualTo(-1);
+    }
+
+    @Test
+    @Order(3)
+    public void examplePart2() {
+        assertThat(Day18.getPart2(Input.fromString(example))).isEqualTo(-1);
+    }
+
+    @Test
+    @Order(4)
+    public void puzzlePart2() {
+        assertThat(Day18.getPart2(Input.forDay(Day18.class))).isEqualTo(-1);
+    }
+}
