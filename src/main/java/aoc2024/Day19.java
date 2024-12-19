4ef8c9da22987d8936b9223ce6c28a3f90334275
alxfhl <110429902+alxfhl@users.noreply.github.com>
2024-12-19 08:20:34 +0100

    Day 19

--- src/main/java/aoc2024/Day19.java
+++ src/main/java/aoc2024/Day19.java
@@ -1,16 +1,49 @@
 package aoc2024;
 
-import java.util.List;
+import java.util.*;
+
+import static java.util.Comparator.comparing;
 
 public class Day19 {
 
     public static long getPart1(List<String> lines) {
-        return 0;
+        Set<String> patterns = new HashSet<>(List.of(lines.getFirst().split(", ")));
+        long count = 0;
+        for (String line : lines) {
+            if (combinations(line, patterns) > 0) {
+                count++;
+            }
+        }
+        return count;
     }
-
 
     public static long getPart2(List<String> lines) {
-        return 0;
+        Set<String> patterns = new HashSet<>(List.of(lines.getFirst().split(", ")));
+        long sum = 0;
+        for (String line : lines) {
+            sum += combinations(line, patterns);
+        }
+        return sum;
     }
+
+    private static long combinations(String line, Set<String> patterns) {
+        Map<String, Long> map = new HashMap<>();
+        map.put(line, 1L);
+        long sum = 0;
+        while (!map.isEmpty()) {
+            String longest = map.keySet().stream().max(comparing(String::length)).stream().findFirst().get();
+            long ways = map.remove(longest);
+            for (String pattern : patterns) {
+                if (longest.equals(pattern)) {
+                    sum += ways;
+                } else if (longest.startsWith(pattern)) {
+                    String rest = longest.substring(pattern.length());
+                    map.compute(rest, (k, v) -> v == null ? ways : v + ways);
+                }
+            }
+        }
+        return sum;
+    }
+
 
 }
--- src/test/java/aoc2024/Day19Test.java
+++ src/test/java/aoc2024/Day19Test.java
@@ -12,30 +12,39 @@ import static org.assertj.core.api.Assertions.assertThat;
 public class Day19Test {
 
     String example = """
+            r, wr, b, g, bwu, rb, gb, br
             
+            brwrr
+            bggr
+            gbbr
+            rrbgbr
+            ubwu
+            bwurrg
+            brgr
+            bbrgwb
             """;
 
     @Test
     @Order(1)
     public void examplePart1() {
-        assertThat(Day19.getPart1(Input.fromString(example))).isEqualTo(-1);
+        assertThat(Day19.getPart1(Input.fromString(example))).isEqualTo(6);
     }
 
     @Test
     @Order(2)
     public void puzzlePart1() {
-        assertThat(Day19.getPart1(Input.forDay(Day19.class))).isEqualTo(-1);
+        assertThat(Day19.getPart1(Input.forDay(Day19.class))).isEqualTo(317);
     }
 
     @Test
     @Order(3)
     public void examplePart2() {
-        assertThat(Day19.getPart2(Input.fromString(example))).isEqualTo(-1);
+        assertThat(Day19.getPart2(Input.fromString(example))).isEqualTo(16);
     }
 
     @Test
     @Order(4)
     public void puzzlePart2() {
-        assertThat(Day19.getPart2(Input.forDay(Day19.class))).isEqualTo(-1);
+        assertThat(Day19.getPart2(Input.forDay(Day19.class))).isEqualTo(883443544805484L);
     }
 }
