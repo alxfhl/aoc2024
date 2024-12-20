d2696fb155497504cee2fbaeb1fd934f1f29184e
alxfhl <110429902+alxfhl@users.noreply.github.com>
2024-12-20 11:14:51 +0100

    Day 20

--- src/main/java/aoc2024/Day20.java
+++ src/main/java/aoc2024/Day20.java
@@ -1,16 +1,120 @@
 package aoc2024;
 
-import java.util.List;
+import aoc2024.tools.CharMatrix;
+import aoc2024.tools.Coord2D;
+import lombok.Data;
+import lombok.EqualsAndHashCode;
+import lombok.ToString;
+
+import java.util.*;
+
+import static java.util.Comparator.comparing;
 
 public class Day20 {
 
-    public static long getPart1(List<String> lines) {
-        return 0;
+    @Data
+    @ToString(exclude = {"neighbors"})
+    @EqualsAndHashCode(onlyExplicitlyIncluded = true)
+    static class Location {
+        @EqualsAndHashCode.Include
+        Coord2D pos;
+        int toStart;
+        Integer toEnd = null;
+        List<Location> neighbors = new ArrayList<>(4);
+
+        public Location(Coord2D pos, int toStart) {
+            this.pos = pos;
+            this.toStart = toStart;
+        }
+
+        public void addNeighbor(Location location) {
+            if (!neighbors.contains(location)) {
+                neighbors.add(location);
+            }
+        }
     }
 
+    public static long getPart1(List<String> lines, int minSaving) {
+        return getPart2(lines, 2, minSaving);
+    }
 
-    public static long getPart2(List<String> lines) {
-        return 0;
+    public static long getPart2(List<String> lines, int cheatTimeLimit, int minSaving) {
+        CharMatrix matrix = CharMatrix.valueOf(lines);
+        Coord2D start = matrix.indexOf('S');
+        Coord2D end = matrix.indexOf('E');
+        matrix.set(start, '.');
+        matrix.set(end, '.');
+        Map<Coord2D, Location> solution = solve(matrix, start, end);
+        return countCheats(solution, cheatTimeLimit, minSaving);
+    }
+
+    private static Map<Coord2D, Location> solve(CharMatrix matrix, Coord2D start, Coord2D end) {
+        var todo = new PriorityQueue<>(comparing(Location::getToStart));
+        Map<Coord2D, Location> solution = new HashMap<>();
+        Location startLocation = new Location(start, 0);
+        solution.put(start, startLocation);
+        todo.add(startLocation);
+        while (!todo.isEmpty()) {
+            Location location = todo.poll();
+            if (location.getPos().equals(end)) {
+                continue;
+            }
+            int toStart = location.getToStart() + 1;
+            for (Coord2D neighbor : location.getPos().getNeighbors()) {
+                if (matrix.get(neighbor) != '.') {
+                    continue;
+                }
+                Location neighborSolution = solution.get(neighbor);
+                if (neighborSolution == null) {
+                    neighborSolution = new Location(neighbor, toStart);
+                    solution.put(neighbor, neighborSolution);
+                    todo.add(neighborSolution);
+                } else if (neighborSolution.getToStart() > toStart) {
+                    neighborSolution.setToStart(toStart);
+                    todo.add(neighborSolution);
+                }
+                neighborSolution.addNeighbor(location);
+                location.addNeighbor(neighborSolution);
+            }
+        }
+        // fill out toEnd
+        todo = new PriorityQueue<>(comparing(Location::getToEnd));
+        solution.get(end).setToEnd(0);
+        todo.add(solution.get(end));
+        while (!todo.isEmpty()) {
+            Location location = todo.poll();
+            int toEnd = location.getToEnd() + 1;
+            for (Location neighbor : location.neighbors) {
+                if (neighbor.getToEnd() == null || neighbor.getToEnd() > toEnd) {
+                    neighbor.setToEnd(toEnd);
+                    todo.add(neighbor);
+                }
+            }
+        }
+        return solution;
+    }
+
+    private static long countCheats(Map<Coord2D, Location> solution, int cheatTimeLimit, int minSaving) {
+        long count = 0;
+        for (Location from : solution.values()) {
+            int fromX = (int) from.getPos().x();
+            int fromY = (int) from.getPos().y();
+            for (int y = fromY - cheatTimeLimit; y <= fromY + cheatTimeLimit; y++) {
+                int distanceY = Math.abs(fromY - y);
+                int remaining = cheatTimeLimit - distanceY;
+                for (int x = fromX - remaining; x <= fromX + remaining; x++) {
+                    Location to = solution.get(new Coord2D(x, y));
+                    if (to == null || to.getToStart() <= from.getToStart()) {
+                        continue;
+                    }
+                    int saving = from.getToEnd() - to.getToEnd() - Math.abs(fromX - x) - distanceY;
+                    if (saving >= minSaving) {
+                        count++;
+                    }
+                }
+            }
+        }
+        return count;
     }
 
 }
--- src/test/java/aoc2024/Day20Test.java
+++ src/test/java/aoc2024/Day20Test.java
@@ -12,30 +12,44 @@ import static org.assertj.core.api.Assertions.assertThat;
 public class Day20Test {
 
     String example = """
-            
+            ###############
+            #...#...#.....#
+            #.#.#.#.#.###.#
+            #S#...#.#.#...#
+            #######.#.#.###
+            #######.#.#...#
+            #######.#.###.#
+            ###..E#...#...#
+            ###.#######.###
+            #...###...#...#
+            #.#####.#.###.#
+            #.#...#.#.#...#
+            #.#.#.#.#.#.###
+            #...#...#...###
+            ###############
             """;
 
     @Test
     @Order(1)
     public void examplePart1() {
-        assertThat(Day20.getPart1(Input.fromString(example))).isEqualTo(-1);
+        assertThat(Day20.getPart1(Input.fromString(example), 1)).isEqualTo(44);
     }
 
     @Test
     @Order(2)
     public void puzzlePart1() {
-        assertThat(Day20.getPart1(Input.forDay(Day20.class))).isEqualTo(-1);
+        assertThat(Day20.getPart1(Input.forDay(Day20.class), 100)).isEqualTo(1197);
     }
 
     @Test
     @Order(3)
     public void examplePart2() {
-        assertThat(Day20.getPart2(Input.fromString(example))).isEqualTo(-1);
+        assertThat(Day20.getPart2(Input.fromString(example), 20, 50)).isEqualTo(285);
     }
 
     @Test
     @Order(4)
     public void puzzlePart2() {
-        assertThat(Day20.getPart2(Input.forDay(Day20.class))).isEqualTo(-1);
+        assertThat(Day20.getPart2(Input.forDay(Day20.class), 20, 100)).isEqualTo(944910);
     }
 }
