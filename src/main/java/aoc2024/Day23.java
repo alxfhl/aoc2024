b078474347326cabe4dcb8e1a713b4aa152c8e3f
alxfhl <110429902+alxfhl@users.noreply.github.com>
2024-12-23 11:43:35 +0100

    Day 23

--- src/main/java/aoc2024/Day23.java
+++ src/main/java/aoc2024/Day23.java
@@ -1,16 +1,96 @@
 package aoc2024;
 
-import java.util.List;
+import lombok.EqualsAndHashCode;
+import lombok.Getter;
+import lombok.RequiredArgsConstructor;
+
+import java.util.*;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
+
+import static java.util.stream.Collectors.toCollection;
 
 public class Day23 {
 
+    @Getter
+    @EqualsAndHashCode(of = "name")
+    @RequiredArgsConstructor
+    static class Computer implements Comparable<Computer> {
+        final String name;
+        final Set<Computer> connections = new HashSet<>();
+
+        @Override
+        public int compareTo(Computer o) {
+            return name.compareTo(o.name);
+        }
+
+        @Override
+        public String toString() {
+            return name;
+        }
+    }
+
     public static long getPart1(List<String> lines) {
-        return 0;
+        Map<String, Computer> computers = parseInput(lines);
+        Set<Set<Computer>> triples = getTriples(computers);
+        return triples.stream()
+                .filter(set -> set.stream().anyMatch(comp -> comp.getName().startsWith("t")))
+                .count();
+    }
+
+    private static Set<Set<Computer>> getTriples(Map<String, Computer> computers) {
+        Set<Set<Computer>> triples = new HashSet<>();
+        for (Computer computer : computers.values()) {
+            for (Computer neighbor : computer.getConnections()) {
+                for (Computer otherNeighbor : neighbor.getConnections()) {
+                    if (computer.getConnections().contains(otherNeighbor)) {
+                        triples.add(Stream.of(computer, neighbor, otherNeighbor)
+                                .collect(toCollection(TreeSet::new)));
+                    }
+                }
+            }
+        }
+        return triples;
+    }
+
+    private static Map<String, Computer> parseInput(List<String> lines) {
+        Map<String, Computer> computers = new HashMap<>();
+        for (String line : lines) {
+            if (line.charAt(2) != '-') {
+                continue;
+            }
+            Computer from = computers.computeIfAbsent(line.substring(0, 2), Computer::new);
+            Computer to = computers.computeIfAbsent(line.substring(3), Computer::new);
+            from.getConnections().add(to);
+            to.getConnections().add(from);
+        }
+        return computers;
     }
 
 
-    public static long getPart2(List<String> lines) {
-        return 0;
+    public static String getPart2(List<String> lines) {
+        Map<String, Computer> input = parseInput(lines);
+        Set<Set<Computer>> networks = getTriples(input);
+        List<Computer> computers = new ArrayList<>(input.values());
+        Set<Computer> biggest = new HashSet<>();
+        while (!networks.isEmpty()) {
+            // System.out.println(networks.size() + " networks of size " + networks.stream().findFirst().get().size());
+            biggest = networks.stream().findFirst().orElseThrow();
+            Set<Set<Computer>> biggerNetworks = new HashSet<>();
+            for (Set<Computer> network : networks) {
+                for (Computer computer : computers) {
+                    if (network.stream().allMatch(pc -> pc.getConnections().contains(computer))) {
+                        Set<Computer> biggerNetwork = new TreeSet<>(network);
+                        biggerNetwork.add(computer);
+                        biggerNetworks.add(biggerNetwork);
+                    }
+                }
+            }
+            networks = biggerNetworks;
+        }
+        return biggest.stream()
+                .map(Computer::getName)
+                .collect(Collectors.joining(","));
     }
 
 }
--- src/test/java/aoc2024/Day23Test.java
+++ src/test/java/aoc2024/Day23Test.java
@@ -12,30 +12,61 @@ import static org.assertj.core.api.Assertions.assertThat;
 public class Day23Test {
 
     String example = """
-            
+            kh-tc
+            qp-kh
+            de-cg
+            ka-co
+            yn-aq
+            qp-ub
+            cg-tb
+            vc-aq
+            tb-ka
+            wh-tc
+            yn-cg
+            kh-ub
+            ta-co
+            de-co
+            tc-td
+            tb-wq
+            wh-td
+            ta-ka
+            td-qp
+            aq-cg
+            wq-ub
+            ub-vc
+            de-ta
+            wq-aq
+            wq-vc
+            wh-yn
+            ka-de
+            kh-ta
+            co-tc
+            wh-qp
+            tb-vc
+            td-yn
             """;
 
     @Test
     @Order(1)
     public void examplePart1() {
-        assertThat(Day23.getPart1(Input.fromString(example))).isEqualTo(-1);
+        assertThat(Day23.getPart1(Input.fromString(example))).isEqualTo(7);
     }
 
     @Test
     @Order(2)
     public void puzzlePart1() {
-        assertThat(Day23.getPart1(Input.forDay(Day23.class))).isEqualTo(-1);
+        assertThat(Day23.getPart1(Input.forDay(Day23.class))).isEqualTo(1119);
     }
 
     @Test
     @Order(3)
     public void examplePart2() {
-        assertThat(Day23.getPart2(Input.fromString(example))).isEqualTo(-1);
+        assertThat(Day23.getPart2(Input.fromString(example))).isEqualTo("co,de,ka,ta");
     }
 
     @Test
     @Order(4)
     public void puzzlePart2() {
-        assertThat(Day23.getPart2(Input.forDay(Day23.class))).isEqualTo(-1);
+        assertThat(Day23.getPart2(Input.forDay(Day23.class))).isEqualTo("av,fr,gj,hk,ii,je,jo,lq,ny,qd,uq,wq,xc");
     }
 }
